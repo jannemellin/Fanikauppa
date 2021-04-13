255a26d3f88fa25878b27bb35ffee5a84943fa02
jannemellin <59602504+jannemellin@users.noreply.github.com>
2021-04-13 16:10:40 +0300

    Add files via upload

---
+++ fanikauppa/precache-manifest.39f59154768f1cb48735f7962a467bf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d419063a44eb8c5cc6939532caa4d53b",
+    "url": "/fanikauppa/index.html"
+  },
+  {
+    "revision": "2f7d2ffa1ff6ef9eabe7",
+    "url": "/fanikauppa/static/css/main.ed4025ee.chunk.css"
+  },
+  {
+    "revision": "e81f27f3a9ef2d51622f",
+    "url": "/fanikauppa/static/js/2.4480cf88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fanikauppa/static/js/2.4480cf88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f7d2ffa1ff6ef9eabe7",
+    "url": "/fanikauppa/static/js/main.85a74182.chunk.js"
+  },
+  {
+    "revision": "8b9bc96a5a9f75a05484",
+    "url": "/fanikauppa/static/js/runtime-main.845ccbcf.js"
+  }
+]);
